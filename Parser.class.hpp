0a783ca5091b4a16f843a9e8e6757d75b7c8c8d0
Dany ARABI <darabi@e1r10p4.42.fr>
2018-02-16 12:47:25 +0100

    Parser

--- Parser.class.hpp
+++ Parser.class.hpp
@@ -4,37 +4,37 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <map>
 class Parser {
 public:
-	Parser(void);
-	Parser(const Parser&);
-	~Parser(void);
-	Parser &operator=(const Parser);
-	void open_file(std::string file);
-	int read_file(std::ifstream *f);
-	int analyze(std::string str);
-	std::string remove_comment(std::string str);
-	int	add_line_to_puzzle(std::string str);
-	void setDim(std::string str);
-	bool is_number(std::string& s);
-	void convert();
-	int display_line(int j, int exp, bool reverse);
-	int display_column(int j, int exp, bool reverse);
-	int check_solvency();
-	void parseOptions(std::string options); 
-	bool checkOptions(char c);
-	std::vector<int> getPuzzle();
-	std::vector<char> getOptions();
-	int getMax();
-	int getDim();
+							Parser(void);
+							Parser(const Parser&);
+							~Parser(void);
+	Parser 					&operator=(const Parser);
+	Puzzle 					*createPuzzle();
+	void 					open_file(std::string file);
+	int 					read_file(std::ifstream *f);
+	int 					analyze(std::string str);
+	std::string 			remove_comment(std::string str);
+	int						add_line_to_puzzle(std::string str);
+	bool 					is_number(std::string& s);
+	void 					convert();
+	int 					display_line(int j, int exp, bool reverse);
+	int 					display_column(int j, int exp, bool reverse);
+	int 					check_solvency();
+	void 					parseOptions(std::string options); 
+	bool 					checkOptions(char c);
+	void 					setDim(std::string str);
+	int 					getDim();
+	int 					getMax();
+	std::vector<int> 		getPuzzle();
+	std::vector<char> 		getOptions();
 
 private:
-	int _row;
-	int _max;
-	int	_dim;
-	std::vector<char> _options;
-	std::vector<int> good_puzzle;
-	std::vector<int> puzzle; //bientot en vecteur
+	int 					_row;
+	int 					_max;
+	int						_dim;
+	std::vector<char> 		_options;
+	std::vector<int> 		good_puzzle;
+	std::vector<int> 		puzzle;
 };
 #endif
