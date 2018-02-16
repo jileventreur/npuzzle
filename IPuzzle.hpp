839fe3057b311a6c60aa5dc046b718476e441e5a
Corentin ADAM <cadam@e1r11p5.42.fr>
2018-02-16 03:15:03 +0100

    interface update

--- IPuzzle.hpp
+++ IPuzzle.hpp
@@ -1,10 +1,12 @@
 class IPuzzle {
 public:
-	virtual int get_case(int x, int y) const = 0;
-	virtual void moove_up(void) = 0;
-	virtual void moove_down(void) = 0;
-	virtual void moove_left(void) = 0;
-	virtual void moove_right(void) = 0;
+	virtual size_t get_case(size_t x, size_t y) const = 0;
+	virtual size_t get_size(void) const = 0;
+	virtual size_t get_blank(void) const = 0;
+	virtual void move_up(void) = 0;
+	virtual void move_down(void) = 0;
+	virtual void move_left(void) = 0;
+	virtual void move_right(void) = 0;
 	virtual std::string toString(void) const = 0;
 
 	virtual ~IPuzzle(void) {}
