67070795bc99afa743fb142fd45732e5cff62487
Corentin ADAM <cadam@e1r11p5.42.fr>
2018-02-16 10:10:44 +0100

    new interface ma guele

--- IPuzzle.hpp
+++ IPuzzle.hpp
@@ -1,13 +1,15 @@
+#ifndef IPUZZLE
+#define IPUZZLE
+#define MAX_NEIGHBOR 4
 class IPuzzle {
 public:
 	virtual size_t get_case(size_t x, size_t y) const = 0;
 	virtual size_t get_size(void) const = 0;
 	virtual size_t get_blank(void) const = 0;
-	virtual void move_up(void) = 0;
-	virtual void move_down(void) = 0;
-	virtual void move_left(void) = 0;
-	virtual void move_right(void) = 0;
+	virtual size_t get_neighbor(std::string & neighbor[MAX_NEIGHBOR]) const = 0;
 	virtual std::string toString(void) const = 0;
+	// virtual IPuzzle *toPuzzle(std::string const &) const = 0; // a voir
 
 	virtual ~IPuzzle(void) {}
 };
+#endif
